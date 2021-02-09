933fda308c760db7f56bb6aed943f98aab6f647f
CrazyCatzzz <g.davide2005@gmail.com>
2021-02-09 09:34:37 +0000

    I don't remember what I changed but it works

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import env from 'dotenv';
 env.config();
-//require('./keepAwake/awake.js');
+
+import config from './settings.json';
+
+import './keepAwake/awake.js';
+
 import { Client } from 'discord.js';
 const client = new Client();
-import config from './settings.json';
+
 import { CommandHandler } from './handlers/command-handler.js';
 const commandHandler = new CommandHandler();
 
@@ -11,6 +15,7 @@ commandHandler.init();
 
 client.on('ready', () => {
   console.log('Bot is ready!')
+  client.user.setActivity(`${config.prefix}help`, { type: "PLAYING" });
 });
 
 client.on('message', async (msg) => {
